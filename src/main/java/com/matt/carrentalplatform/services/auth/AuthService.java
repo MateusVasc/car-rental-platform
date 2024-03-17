43ea820a269d556f182fd3327b422eda85a02af1
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-03-17 20:37:31 -0300

    adding AuthService interface

---
+++ src/main/java/com/matt/carrentalplatform/services/auth/AuthService.java
@@ -0,0 +1,9 @@
+package com.matt.carrentalplatform.services.auth;
+
+import com.matt.carrentalplatform.dto.SignupRequest;
+import com.matt.carrentalplatform.dto.UserDTO;
+
+public interface AuthService {
+
+  UserDTO createCustumer(SignupRequest signupRequest);
+}
